88651e5c2238fe15ed08e4c5fc8cddf1d10778a5
Nora Söderlund <norasoderlund@icloud.com>
2022-10-07 14:14:39 +0200

    Added all context methods and properties

--- API/Context.jsx
+++ API/Context.jsx
@@ -10,34 +10,114 @@ export default class ContextAPI extends BundleAPI {
         // for the bundle api
         this._webView = this._canvas._webView;
     };
-
-    async clearRect(x, y, w, h) {
-        await this._addToBundleOrInject(`
-            ${this._context}.clearRect(${x}, ${y}, ${w}, ${h});
-        `);
-    };
-
-    set fillStyle(value) {
-        this._addToBundleOrInject(`
-            ${this._context}.fillStyle = "${value}";
-        `);
-    };
-    
-    async fillRect(x, y, w, h) {
-        await this._addToBundleOrInject(`
-            ${this._context}.fillRect(${x}, ${y}, ${w}, ${h});
-        `);
-    };
-    
-    async clearRect(x, y, w, h) {
-        await this._addToBundleOrInject(`
-            ${this._context}.clearRect(${x}, ${y}, ${w}, ${h});
-        `);
-    };
-
-    set globalAlpha(value) {
-        this._addToBundleOrInject(`
-            ${this._context}.globalAlpha = ${value};
-        `);
-    };
 };
+
+const properties = [
+    "direction",
+    "fillStyle",
+    "filter",
+    "font",
+    "fontKerning",
+    "fontStretch",
+    "fontVariantCaps",
+    "globalAlpha",
+    "globalCompositeOperation",
+    "imageSmoothingEnabled",
+    "imageSmoothingQuality",
+    "letterSpacing",
+    "lineCap",
+    "lineDashOffset",
+    "lineJoin",
+    "lineWidth",
+    "miterLimit",
+    "shadowBlur",
+    "shadowColor",
+    "shadowOffsetX",
+    "shadowOffsetY",
+    "strokeStyle",
+    "textAlign",
+    "textBaseline",
+    "textRendering",
+    "wordSpacing"    
+];
+
+for(let index = 0; index < properties.length; index++) {
+    const property = properties[index];
+
+    Object.defineProperty(ContextAPI.prototype, property, {
+        set(...args) {
+            let json = JSON.stringify([...args]);
+
+            this._addToBundleOrInject(`
+                ${this._context}.${property} = ${json.substring(1, json.length - 1)};
+            `);
+        },
+
+        async get() {
+            return await this._addToBundleOrInject(`
+                ${this._context}.${property};
+            `);
+        }
+    });
+}
+
+const methods = [
+    "arc",
+    "arcTo",
+    "beginPath",
+    "bezierCurveTo",
+    "clearRect",
+    "clip",
+    "closePath",
+    "createConicGradient",
+    "createImageData",
+    "createLinearGradient",
+    "createPattern",
+    "createRadialGradient",
+    "drawFocusIfNeeded",
+    "drawImage",
+    "ellipse",
+    "fill",
+    "fillRect",
+    "fillText",
+    "getContextAttributes",
+    "getImageData",
+    "getLineDash",
+    "getTransform",
+    "isContextLost",
+    "isPointInPath",
+    "isPointInStroke",
+    "lineTo",
+    "measureText",
+    "moveTo",
+    "putImageData",
+    "quadraticCurveTo",
+    "rect",
+    "reset",
+    "resetTransform",
+    "restore",
+    "rotate",
+    "roundRect",
+    "save",
+    "scale",
+    "scrollPathIntoView",
+    "setLineDash",
+    "setTransform",
+    "stroke",
+    "strokeRect",
+    "strokeText",
+    "transform",
+    "translate"  
+];
+
+for(let index = 0; index < methods.length; index++) {
+    const method = methods[index];
+
+    ContextAPI.prototype[method] = function(...args) {
+        let json = JSON.stringify([...args]);
+
+        this._addToBundleOrInject(`
+            ${this._context}.${method}(${json.substring(1, json.length - 1)});
+        `);
+    };
+}
