e3da2d77560fcca03604ef2ee1d66c1315e8fa6d
Nora Söderlund <norasoderlund@icloud.com>
2022-10-07 13:29:29 +0200

    Added a bundle execution test

--- API/Context.jsx
+++ API/Context.jsx
@@ -1,35 +1,64 @@
+import { TouchableHighlightBase } from "react-native";
+
 export default class ContextAPI {
     constructor(canvas, context) {
         this._canvas = canvas;
         this._context = context;
     };
 
-    async clearRect(x, y, w, h) {
+    _bundle = null;
+
+    startBundle() {
+        this._bundle = [];
+    };
+
+    async executeBundle() {
         await this._canvas._webView.current.injectJavaScript(`
+            {
+                ${this._bundle.join("")}
+            }
+        `);
+
+        this.stopBundle();
+    };
+
+    async stopBundle() {
+        this._bundle = null;
+    };
+
+    async _addToBundleOrInject(message) {
+        if(this._bundle)
+            this._bundle.push(message);
+        else
+            await this._canvas._webView.current.injectJavaScript(message);
+    };
+
+    async clearRect(x, y, w, h) {
+        await this._addToBundleOrInject(`
             ${this._context}.clearRect(${x}, ${y}, ${w}, ${h});
         `);
     };
 
     set fillStyle(value) {
-        this._canvas._webView.current.injectJavaScript(`
+        this._addToBundleOrInject(`
             ${this._context}.fillStyle = "${value}";
         `);
     };
     
     async fillRect(x, y, w, h) {
-        await this._canvas._webView.current.injectJavaScript(`
+        await this._addToBundleOrInject(`
             ${this._context}.fillRect(${x}, ${y}, ${w}, ${h});
         `);
     };
     
     async clearRect(x, y, w, h) {
-        await this._canvas._webView.current.injectJavaScript(`
+        await this._addToBundleOrInject(`
             ${this._context}.clearRect(${x}, ${y}, ${w}, ${h});
         `);
     };
 
     set globalAlpha(value) {
-        this._canvas._webView.current.injectJavaScript(`
+        this._addToBundleOrInject(`
             ${this._context}.globalAlpha = ${value};
         `);
     };
